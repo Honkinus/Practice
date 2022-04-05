9d389b4ca39c1d6f3002365db0c30ca5015d48d5
Elliot <83696706+Honkinus@users.noreply.github.com>
2022-04-05 22:09:35 +0300

    Add files via upload

---
+++ Section 7.py
@@ -0,0 +1,15 @@
+import math
+# Section 7 Math Functions
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+print(round(pi))
+print(math.ceil(pi))
+print(math.floor(pi))
+print(abs(pi))
+print(pow(pi, 2))
+print(math.sqrt(pi))
+print(max(x,y,z))
+print(min(x,y,z))
